002bff486fbc0c7977581f28ca5d3c39d95e82e3
ramilka <lilya1976.03.03@gmail.com>
2024-09-21 06:29:58 +0600

    Создал все необходимое для логина и регистрации.

---
+++ api/routers/users.ts
@@ -0,0 +1,63 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import auth, { RequestWithUser } from '../middleware/auth';
+import User from "../models/User";
+
+const usersRouter = express.Router();
+
+usersRouter.post('/', async (req, res, next) => {
+    try {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password,
+        });
+
+        user.generateToken();
+
+        await user.save();
+        return res.send(user);
+    } catch (error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+            return res.status(400).send(error);
+        }
+
+        return next(error);
+    }
+});
+
+usersRouter.post('/sessions', async (req, res, next) => {
+    try {
+        const user = await User.findOne({username: req.body.username});
+
+        if (!user) {
+            return res.status(400).send({error: 'Username not found!'});
+        }
+
+        const isMatch = await user.checkPassword(req.body.password);
+
+        if (!isMatch) {
+            return res.status(400).send({error: 'Password is wrong!'});
+        }
+
+        user.generateToken();
+        await user.save();
+
+        return res.send(user);
+    } catch (error) {
+        return next(error);
+    }
+});
+
+usersRouter.post('/secret', auth, async (req: RequestWithUser, res, next) => {
+    try {
+        if (!req.user) {
+            return res.status(401).send({error: 'User not found'});
+        }
+
+        return res.send('Secret text, username=' + req.user?.username);
+    } catch (error) {
+        return next(error);
+    }
+});
+
+export default usersRouter;
